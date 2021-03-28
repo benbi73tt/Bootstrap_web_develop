1ff18f777d38f8d00ae991ee7428cf3f6af1ff16
Anton <56966986+benbi73tt@users.noreply.github.com>
2021-03-28 11:23:37 +0400

    main bootstrap

---
+++ bootstrap_web_devolop/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <Button variant='primary'>Web develop</Button>
+  );
+}
+
+export default App;
